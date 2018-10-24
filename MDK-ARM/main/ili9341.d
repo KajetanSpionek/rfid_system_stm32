d9f32c8c996b1f4001075f2e3d5f69da9d2c35b4
Kajetan Spionek <kajetanspionek@gmail.com>
2018-10-24 21:00:40 +0200

    Drivers and util added

---
+++ MDK-ARM/main/ili9341.d
@@ -0,0 +1,4 @@
+main\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.c
+main\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.h
+main\ili9341.o: ..\Drivers\BSP\Components\ili9341\../Common/lcd.h
+main\ili9341.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/main/stmpe811.d
@@ -0,0 +1,5 @@
+main\stmpe811.o: ..\Drivers\STMPE811\stmpe811.c
+main\stmpe811.o: ..\Drivers\STMPE811\stmpe811.h
+main\stmpe811.o: ..\Drivers\STMPE811\ts.h
+main\stmpe811.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+main\stmpe811.o: ..\Drivers\STMPE811\io.h
